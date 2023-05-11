bad0c7edf387e3b1dccd14b829c0bec393518695
Allie Soldau <alliesoldau@gmail.com>
2023-05-11 18:45:20 -0400

    got rid of quotes in strings in sql table

--- convert_data.py
+++ convert_data.py
@@ -23,6 +23,7 @@ for row in client_records: # this will do it just for the first entry
     name = tuple(row[c] for c in column_client_records_counselor_name)
     names = name[0]
     namesArray = names.split(",")
+    uniqueNames = []
     # this splits up any duplicate counselors and puts them into the counselors table independently
     if len(namesArray) > 1:
         counter = 0
@@ -30,17 +31,20 @@ for row in client_records: # this will do it just for the first entry
         while counter < len(namesArray):
             currentName = namesArray[counter]
             # initializing bad_chars_list
-            bad_chars = [';', ':', '!', "*", "[", "]", ","]
+            bad_chars = [';', ':', '!', "*", "[", "]", ",", '" ', ' "', '"']
             # using replace() to remove bad_chars
             for i in bad_chars:
                 currentName = currentName.replace(i, '')
-            # add counselor to the table
-            cursor.execute('insert into Counselors values (?)', (currentName,))
+            # update the unique names array if you find a novel name and add it to the table    
+            if currentName not in uniqueNames:
+                uniqueNames.append(currentName)
+                cursor.execute('insert into Counselors values (?)', (currentName,))
             counter += 1
     # string literals should be single quotes not double qoutes (https://www.sqlite.org/quirks.html point 8)
     # TO DO: clean the data now that you can grad individual items
     # TO DO: ^^ see example above for how to grad just timestamp, then figure out how to manipulate it
     # NOTICE: arrays seem to always have quotes, what's up with that
+    print(uniqueNames)
     cursor.execute('insert into ClientRecords values (?,?,?,?,?,?,?,?,?,?)', value)
 
 
