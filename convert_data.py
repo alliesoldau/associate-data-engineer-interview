c6136213e7177c09369ec75afcd97e52b3e9f00a
Allie Soldau <alliesoldau@gmail.com>
2023-05-11 20:14:08 -0400

    added initial counselor id and total transfers to contact table. now I need to clean up the other data so that it's useable

--- convert_data.py
+++ convert_data.py
@@ -7,7 +7,7 @@ cursor = connection.cursor()
 # FOR RAW DATA FULL TABLE
 # cursor.execute('Create Table if not exists ClientRecords (time_call_began datetime, time_call_ended datetime, counselors string, transfer_timestamps datetime, issues_discussed string, call_rating integer, initial_risk_level integer, client_pronouns string, client_name string, client_location string)')
 cursor.execute('Create Table if not exists Counselors (id integer primary key, name string)')
-cursor.execute('Create Table if not exists Transfers (id integer primary key, contact_id integer, counselor_id integer, timestamp datetime)')
+cursor.execute('Create Table if not exists Contacts (id integer primary key, time_call_began datetime, time_call_ended datetime, issues_discussed string, call_rating integer, initial_risk_level integer, client_pronouns string, client_name string, client_location string, initial_counelor_id integer, total_transfers integer)')
 
 # read through each line in the JSONL file and append it to the client_records array 
 client_records = []
@@ -17,15 +17,16 @@ with open('client_records.json') as f:
 
 # define columns 
 # FOR RAW DATA FULL TABLE # columns_client_records = ['time_call_began','time_call_ended','counselors', 'transfer_timestamps', 'issues_discussed','call_rating','initial_risk_level','client_pronouns', 'client_name', 'client_location']
-column_client_records_counselor_name = ['counselors']
+columns_counselors = ['counselors']
+columns_contacts = ['time_call_began','time_call_ended', 'issues_discussed', 'call_rating', 'initial_risk_level', 'client_pronouns', 'client_name', 'client_location']
 # loop through client_records and add data to our table 
 # DRAW BACK: if 2 counselors have the same name there's no way to know if they're dulpicates or individuals. A unique counselor ID supplied by Trevor would help
 uniqueNames = []
 for row in client_records:
-    # FOR RAW DATA FULL TABLE # value = tuple(row[c] for c in columns_client_records)
-    name = tuple(row[c] for c in column_client_records_counselor_name)
+    # counselor data
+    name = tuple(row[c] for c in columns_counselors)
     names = name[0].split(",")
-    # namesArray = name[0].split(",")
+    total_transfers = (len(names))
     # add unique counselors to the table
     currentName = []
     if len(names) > 0:
@@ -38,10 +39,20 @@ for row in client_records:
             for i in bad_chars:
                 currentName = currentName.replace(i, '')
             # update the unique names array if you find a novel name and add it to the table    
+            if counter == 0:
+                initial_counselor = currentName
             if currentName not in uniqueNames:
                 uniqueNames.append(currentName)
                 cursor.execute('insert into Counselors(name) values (?)', (currentName,))
             counter += 1
+    # contacts data
+    contact = list(row[c] for c in columns_contacts)
+    cursor.execute('SELECT id FROM Counselors WHERE name=?', (initial_counselor,))
+    initial_counselor_tuple = cursor.fetchall()
+    initial_counselor_id = initial_counselor_tuple[0]
+    contact.append(initial_counselor_id[0])
+    contact.append(total_transfers)
+    cursor.execute('insert into Contacts(time_call_began, time_call_ended, issues_discussed, call_rating, initial_risk_level, client_pronouns, client_name, client_location, initial_counelor_id, total_transfers) values (?,?,?,?,?,?,?,?,?,?)', contact)
     # TO DO: clean the data now that you can grad individual items
     # NOTICE: arrays seem to always have quotes, what's up with that
     # FOR RAW DATA FULL TABLE # cursor.execute('insert into ClientRecords values (?,?,?,?,?,?,?,?,?,?)', value)
